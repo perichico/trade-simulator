b4ff4785dececb1416653084cc4d2a61081dc4de
perichico <perifedea000@gmail.com>
2025-04-14 12:56:50 +0200

    init script

---
+++ start_servers.bat
@@ -0,0 +1,10 @@
+@echo off
+echo Starting Backend Server...
+cd c:\Users\perif\Documents\GitHub\trade-simulator\sources\backend
+start cmd /k "node --watch app.js"
+
+echo Starting Angular Frontend...
+cd c:\Users\perif\Documents\GitHub\trade-simulator\sources\frontendAngular
+start cmd /k "ng serve"
+
+echo Servers are starting...
